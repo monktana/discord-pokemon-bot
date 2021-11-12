4fd5db67d3dd6005ed8b3214a8a431c743a53a76
monktana <svengiebelmann@gmail.com>
2021-11-12 15:02:47 +0100

    feat(events): first implementation of event handler

--- src/index.js
+++ src/index.js
@@ -12,24 +12,16 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
 	}
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
+}
 
 client.login(TOKEN);
