1bb4611da71b05adcce4bba57e6807dec8b23b6a
monktana <svengiebelmann@gmail.com>
2021-11-19 14:43:00 +0100

    feat(language): extending language files and functionality

--- src/commands/pokedex.js
+++ src/commands/pokedex.js
@@ -2,24 +2,24 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const PokemonService = require('../services/pokemon-service.js');
 const colors = require('../utils/colors');
-const LanguageManager = require('../utils/translate.js');
+const language = require('../utils/translate.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pokedex')
-		.setDescription('Shows the pokédex entry of a pokémon.')
+		.setDescription(language.get('command_pokedex_description'))
 		.addStringOption(option =>
-			option.setName('pokemon')
-				.setDescription('Name of the pokémon')
+			option.setName(language.get('option_pokemon_name'))
+				.setDescription(language.get('option_pokemon_description'))
 				.setRequired(true),
 		),
 	async execute(interaction) {
 		await interaction.deferReply();
 
-		const pokemon = interaction.options.getString('pokemon');
-		const searchTerm = LanguageManager.translate(pokemon.toLowerCase());
+		const pokemon = interaction.options.getString(language.get('option_pokemon_name'));
+		const searchTerm = language.translate(pokemon.toLowerCase());
 
-		const { height, weight, name, types, stats, sprites } = await PokemonService.getPokemon(searchTerm);
+		const { height, weight, types, stats, sprites } = await PokemonService.getPokemon(searchTerm);
 		const color = types[0].type.name;
 
 		const embed = new MessageEmbed()
@@ -27,10 +27,10 @@ module.exports = {
 			.setTitle(pokemon)
 			.setThumbnail(sprites.front_default)
 			.addFields(
-				{ name: 'Height', value: `${height}`, inline: true },
-				{ name: 'Weight', value: `${weight}`, inline: true },
-				{ name: 'Types', value: types.map(type => type.type.name).join(', ') },
-				{ name: 'Base stats', value: stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join('\r\n') },
+				{ name: language.get('height'), value: `${height}`, inline: true },
+				{ name: language.get('weight'), value: `${weight}`, inline: true },
+				{ name: language.get('types'), value: types.map(type => language.get(type.type.name)).join(', ') },
+				{ name: language.get('base_stats'), value: stats.map(stat => `${language.get(stat.stat.name)}: ${stat.base_stat}`).join('\r\n') },
 			);
 		await interaction.editReply({ embeds: [embed] });
 	},
--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,5 +1,10 @@
+const messages = require('../languages/core');
 const pokemon = require('../languages/pokemon');
 
 module.exports.translate = function(name, target = 'en') {
 	return pokemon[name][target];
+};
+
+module.exports.get = function(key, target = 'de') {
+	return messages[key][target];
 };
