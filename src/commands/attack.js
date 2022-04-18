98a01b77029221125bf736d417b83aa5aa6f6774
monktana <svengiebelmann@gmail.com>
2022-04-18 21:58:35 +0200

    feat: moved response to language file

--- src/commands/attack.js
+++ src/commands/attack.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { parseEffectiveness, Language } = require('../utils/utils');
 const PokemonService = require('../services/pokemon-service');
@@ -8,16 +9,16 @@ module.exports = {
 		.setDescription(Language.lookup('command.attack.description'))
 		.addStringOption(option => option.setName(Language.lookup('option.pokemon.name'))
 			.setDescription(Language.lookup('option.pokemon.description'))
-			.setRequired(true)
+			.setRequired(true),
 		)
 		.addStringOption(option => option.setName(Language.lookup('option.type.first'))
 			.setDescription(Language.lookup('option.type.description'))
-			.setRequired(true)
+			.setRequired(true),
 		),
-		
+
 	async execute(interaction) {
 		await interaction.deferReply();
-		
+
 		const pokemonParameter = interaction.options.getString(Language.lookup('option.pokemon.name'), true);
 		const typeParameter = interaction.options.getString(Language.lookup('option.type.first'), true);
 
@@ -25,14 +26,14 @@ module.exports = {
 		const type = (await PokemonService.getType(typeParameter.toLowerCase())).results[0];
 
 		const effectiveness = type.matchups.reduce((accumulator, matchup) => {
-			if (!pokemon.types.some(type => type.name === matchup.name)) {
-        return accumulator;
-      }
+			if (!pokemon.types.some((pokemonType) => pokemonType.name === matchup.name)) {
+				return accumulator;
+			}
 
 			return accumulator *= matchup.matchup.effectiveness;
 		}, 1);
 
-    const reply = `Attacking ${pokemon.name} with a(n) ${type.name} attack is: ${parseEffectiveness(effectiveness)} (${effectiveness}x)`; 
+		const reply = util.format(Language.lookup('command.attack.answer'), pokemon.name, type.name, parseEffectiveness(effectiveness), effectiveness);
 
 		await interaction.editReply({ content: reply });
 	},
